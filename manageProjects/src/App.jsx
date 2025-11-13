d6ce41fbf61289a98f79d99b6e5fee42291d1589
Elizabeth Cruz <elizabeth.cruz.efinfo@gmail.com>
2025-11-12 19:59:59 -0600

    add functionality in buttons to create a new project

--- manageProjects/src/App.jsx
+++ manageProjects/src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+
 import ProjectsSideBar from "./components/ProjectsSideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+     projects: [],
+  });
+
+  function hanldAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: null,
+    }));
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected addProject={hanldAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NewProject />
+      <ProjectsSideBar addProject={hanldAddProject} />
+      {content}
     </main>
   );
 }
--- manageProjects/src/components/ProjectsSideBar.jsx
+++ manageProjects/src/components/ProjectsSideBar.jsx
@@ -1,11 +1,13 @@
-export default function ProjectsSideBar() {
+import Button from "./Button";
+
+export default function ProjectsSideBar({addProject}) {
     return (
         <aside className="w-13 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
             <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
             <div>
-                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+                <Button onClick={addProject}>
                    + Add Project
-                </button>
+                </Button>
             </div>
         </aside>
     );
