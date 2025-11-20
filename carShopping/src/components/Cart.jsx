ff44feebe576788a546b4ca01de1751556bbb2ae
Elizabeth Cruz <elizabeth.cruz.efinfo@gmail.com>
2025-11-19 19:59:39 -0600

    learning how to add context API in the project

--- carShopping/src/components/Cart.jsx
+++ carShopping/src/components/Cart.jsx
@@ -1,4 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context.tsx";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const {items} = useContext(CartContext);
+  //items = cartCtx.items;
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
