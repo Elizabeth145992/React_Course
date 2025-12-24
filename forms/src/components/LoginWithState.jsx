0355003eda4737031f93cd8f0d61463f446d131f
Elizabeth Cruz <elizabeth.cruz.efinfo@gmail.com>
2025-12-23 19:57:56 -0600

    add new project to practice forms in apps react

---
+++ forms/src/components/LoginWithState.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: ""
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitted values:", enteredValues);
+  }
+
+  function handleValueChange(field, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [field]: value
+    }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleValueChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleValueChange('password', event.target.value)}
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button type="button" className="button" onClick={handleSubmit}>
+          Login
+        </button>
+      </p>
+    </form>
+  );
+}
